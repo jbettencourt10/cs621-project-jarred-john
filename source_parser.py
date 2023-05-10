d6215b55be752c67cc274ed278dfcd3867fdd03e
Joseph Black <17530522+jbinvnt@users.noreply.github.com>
2023-05-10 15:12:47 -0500

    Experiment with its behavior under excessive parameters

--- source_parser.py
+++ source_parser.py
@@ -1,12 +1,16 @@
 import re
 
-def parse_signature(source_code):
+def parse_signature(source_code, max_params=10):
     type_name = "[a-zA-Z0-9\_]+"
     variable_name = "[a-zA-Z0-9\_]+"
     space = "\s+"
     any_space = "[\\n\\r\s]*"
     beginning_characters = "[^a-zA-Z0-9\_]*"
-    regex_string = f"{beginning_characters}({type_name}{space}{variable_name}){any_space}\\((?:({any_space}{type_name}{space}{variable_name}){any_space},{any_space})*{any_space}({type_name}{space}{variable_name})?\\){any_space}\\{{.*"
+    declaration = f"{type_name}{space}{variable_name}"
+    parameter = f"{type_name}{space}{variable_name}"
+    non_last_parameter = f"(?:{any_space}({parameter}),{any_space})"
+    last_parameter = f"(?:{any_space}({parameter}){any_space})"
+    regex_string = f"{beginning_characters}({declaration}){any_space}\\({(non_last_parameter + '?')*(max_params-1)}{last_parameter}?\\){any_space}\\{{.*"
     regex = re.compile(regex_string)
     result_tuples = regex.findall(source_code)
     result_list = list(map(lambda signature_tuple: list(signature_tuple), result_tuples))
--- test_source_parser.py
+++ test_source_parser.py
@@ -1,6 +1,12 @@
 from unittest import TestCase
 import source_parser
 
-class ParserTestCase(TestCase):
+class BasicParamsCase(TestCase):
+    def test_no_params(self):
+        self.assertListEqual([["int myFunc", "", "", ""]], source_parser.parse_signature("int myFunc() {", 3))
     def test_simple_params(self):
-        self.assertListEqual([["int myFunc", "bool myVarA", "String myVarB"]], source_parser.parse_signature("int myFunc(bool myVarA, String myVarB) {"))
+        self.assertListEqual([["int myFunc", "bool myVarA", "String myVarB"]], source_parser.parse_signature("int myFunc(bool myVarA, String myVarB) {", 2))
+    def test_too_many_params(self):
+        self.assertListEqual([["int myFunc", "bool myVarA", "String myVarB"]], source_parser.parse_signature("int myFunc(bool myVarA, String myVarB, float myVarC) {", 2))
+    def test_not_enough_params(self):
+        self.assertListEqual([["int myFunc", "bool myVarA", "String myVarB", "float myVarC", "", "", ""]], source_parser.parse_signature("int myFunc(bool myVarA, String myVarB, float myVarC) {", 6))
