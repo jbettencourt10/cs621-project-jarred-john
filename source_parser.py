591dc9d753d4b2b4b5e0305d21399d8f10d514ae
Joseph Black <17530522+jbinvnt@users.noreply.github.com>
2023-05-15 13:19:13 -0500

    Work on supporting multiple template parameters and switch to faster regex module

--- source_parser.py
+++ source_parser.py
@@ -1,17 +1,21 @@
-import re
+import regex
+from pathlib import Path
 
 def parse_signature(source_code, max_params=10):
-    type_name = "[a-zA-Z0-9\_:<>]+"
-    variable_name = "[a-zA-Z0-9\_:<>]+"
     space = "\s+"
     any_space = "[\\n\\r\s]*"
+    type_name = f"(?:[a-zA-Z0-9\_:]+(?:<(?:{any_space}[a-zA-Z0-9\_:]+,?)+>)?)"
+    variable_name = "[a-zA-Z0-9\_:]+"
     beginning_characters = "[^a-zA-Z0-9\_]*"
-    #modifiers = f"(?:const)|[*&]" # TODO
-    #declaration = f"(?:const{space}(?:[&\*]{any_space})?)?{type_name}{any_space}[&\*]?{space}[&\*]?{variable_name}"
     declaration = f"{type_name}{any_space}[&\*]*{space}[&\*]*{variable_name}"
     parameter = lambda capture: f"(?:{any_space}(?:const{space})?{('(' if capture else '') + declaration + (')' if capture else '')},?{any_space})"
     regex_string = f"{beginning_characters}(?:const{space})?({declaration}){any_space}\\({(parameter(True) + '?')*(max_params)}{parameter(False)}*\\){any_space}(?:const{any_space})?\\{{.*"
-    regex = re.compile(regex_string)
-    result_tuples = regex.findall(source_code)
+    expression = regex.compile(regex_string)
+    result_tuples = expression.findall(source_code)
     result_list = list(map(lambda signature_tuple: list(signature_tuple), result_tuples))
     return result_list
+if __name__ == "__main__":
+    #with open("VBORenderer.cc", "r") as source_file:
+    text_contents = Path("VBORendererPreprocessed.cc").read_text()
+    results = parse_signature(text_contents, 6)
+    print(results)
