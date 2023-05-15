fe2a0efb9abeeed90d91f2ce25e601962522b096
Joseph Black <17530522+jbinvnt@users.noreply.github.com>
2023-05-15 12:41:17 -0500

    Get the const test to pass

--- test_source_parser.py
+++ test_source_parser.py
@@ -25,8 +25,8 @@ class MultiLineCase(TestCase):
     def test_function_call_in_body(self):
         self.assertListEqual([["int myFunc1", "bool myVarA", "String myVarB", "float myVarC", "", "", ""], ["arr myFunc2", "double otherVarA", "bool otherVarB", "", "", "", ""]], source_parser.parse_signature("int myFunc1(bool myVarA, String myVarB, float myVarC) {\n    otherFunctionCall(x, y, zVariable, 1);\n}\n  arr myFunc2( double otherVarA, bool otherVarB)\n{\n}", 6))
 class OpenScadCase(TestCase):
-    def test_complex_const(self):
-        self.assertListEqual(["size_t VBORenderer::getSurfaceBufferSize", "std::shared_ptr<CSGProducts>& products", "bool highlight_mode", "bool background_mode", "bool unique_geometry"], source_parser.parse_signature("size_t VBORenderer::getSurfaceBufferSize(const std::shared_ptr<CSGProducts>& products, bool highlight_mode, bool background_mode, bool unique_geometry) const\n{\n  size_t buffer_size = 0;", 4))
+    def test_const_and_complex_declr(self):
+        self.assertListEqual([["size_t VBORenderer::getSurfaceBufferSize", "std::shared_ptr<CSGProducts>& products", "bool highlight_mode", "bool background_mode", "bool unique_geometry"]], source_parser.parse_signature("size_t VBORenderer::getSurfaceBufferSize(const std::shared_ptr<CSGProducts>& products, bool highlight_mode, bool background_mode, bool unique_geometry) const\n{\n  size_t buffer_size = 0;", 4))
 
 # TODO const and pointers and array types
 # TODO there should be tests for public and static modifiers to function declarations.
